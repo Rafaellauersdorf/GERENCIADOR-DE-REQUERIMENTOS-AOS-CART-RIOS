34b0b3e9632f2aa2399d834dad906dedcba39e8c
Rafael Souza <117686048+Rafaellauersdorf@users.noreply.github.com>
2024-07-30 08:34:27 -0300

    Update mod1.py

--- mod1.py
+++ mod1.py
@@ -1,13 +1,16 @@
 import customtkinter
+from customtkinter import CTkImage
 import os
-from PIL import Image, ImageTk, ImageEnhance
+from PIL import Image, ImageEnhance
+import sys
+
 
 def abrir_arquivo():
     opcao_selecionada = opcoes.get()
     pasta_destino = ""
 
     if opcao_selecionada == "1 - Arrazoado Leis IAP/IAT e REQ. Modelo":
-        pasta_destino = "S:/GEAD-DAG-PATRIMONIO/REGISTRO DE IMOVEIS IAT - GERAL/LEIS E DECRETOS ATUAIS PARA REQUERIMENTO DE REGISTROS/ARRAZOADO DE LEIS CRIAÇÃO IAP ATE IAT - NÃO MUDA CNPJ" #PASTA ONDE CONTEM OS ARQUIVOS A SER EDITADOS.
+        pasta_destino = "S:/GEAD-DAG-PATRIMONIO/REGISTRO DE IMOVEIS IAT - GERAL/LEIS E DECRETOS ATUAIS PARA REQUERIMENTO DE REGISTROS/ARRAZOADO DE LEIS CRIAÇÃO IAP ATE IAT - NÃO MUDA CNPJ"
     elif opcao_selecionada == "2 - Arrazoado Leis ITCG/IAT e REQ. Modelo":
         pasta_destino = "S:/GEAD-DAG-PATRIMONIO/REGISTRO DE IMOVEIS IAT - GERAL/LEIS E DECRETOS ATUAIS PARA REQUERIMENTO DE REGISTROS/ARRAZOADO DE LEIS CRIAÇÃO ITCG ATE IAT - MUDA CNPJ"
     elif opcao_selecionada == "3 - Arrazoado Leis ITC/IAT e REQ. Modelo":
@@ -65,10 +68,21 @@ def atualizar_planilha():
 app = customtkinter.CTk()
 app.geometry("650x350")
 app.title("Programa Modelo de Pedidos aos Cartórios")
+app.grid_rowconfigure(0, weight=1)
+app.grid_columnconfigure((0, 1), weight=1)
+
+def resource_path(relative_path):
+    """Obtém o caminho absoluto para um recurso, considerando se o script está sendo executado como um arquivo .py ou dentro de um executável."""
+    try:
+        # PyInstaller cria uma variável _MEIPASS para o diretório temporário
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 # Carregar e ajustar a imagem de fundo
-imagem_fundo = Image.open(r"C:\Users\bol.rafaelsouza\Desktop\Docs-Rafael-Temporários\Programa-modelo-pedidos-cartorios\Logo animado azul neon futurista para tecnologia.png")  # Substitua pelo caminho da sua imagem
-largura_imagem = 180  # Largura desejada para a imagem na lateral
+imagem_fundo = Image.open(resource_path("Logo animado azul neon futurista para tecnologia.png"))
+largura_imagem = 270  # Largura desejada para a imagem na lateral
 altura_imagem = imagem_fundo.height * largura_imagem // imagem_fundo.width  # Ajusta a altura proporcionalmente
 imagem_fundo = imagem_fundo.resize((largura_imagem, altura_imagem))  # Redimensiona a imagem
 
@@ -78,12 +92,12 @@ imagem_fundo = enhancer_cor.enhance(0.5)  # Ajuste o valor para controlar a satu
 enhancer_contraste = ImageEnhance.Contrast(imagem_fundo)
 imagem_fundo = enhancer_contraste.enhance(0.7)  # Ajuste o valor para controlar o contraste (1.0 = original)
 
-# Converter para PhotoImage (sem transparência)
-imagem_fundo_tk = ImageTk.PhotoImage(imagem_fundo)
+# Converter para CTkImage
+imagem_fundo_ctk = CTkImage(light_image=imagem_fundo, size=(largura_imagem, altura_imagem))
 
 # Criar um rótulo para exibir a imagem de fundo
-label_fundo = customtkinter.CTkLabel(app, image=imagem_fundo_tk, text="")
-label_fundo.place(x=0, y=(app.winfo_height() - altura_imagem) // 2)  # Centraliza verticalmente
+label_fundo = customtkinter.CTkLabel(app, image=imagem_fundo_ctk, text="")  # Use imagem_fundo_ctk aqui
+label_fundo.place(x=0, y=(app.winfo_height() // 2) - (altura_imagem // 2))  # Centraliza verticalmente
 
 opcoes = customtkinter.CTkComboBox(app, 
                                   values=[
@@ -95,7 +109,7 @@ opcoes = customtkinter.CTkComboBox(app,
 opcoes.pack(pady=20)
 
 botao_abrir = customtkinter.CTkButton(app, text="Abrir", command=abrir_arquivo)
-botao_abrir.pack(pady=10)
+botao_abrir.pack(pady=5)
 
 botao_criar_pasta = customtkinter.CTkButton(app, text="Criar Nova Pasta", command=criar_pasta)
 botao_criar_pasta.pack(pady=5)
@@ -105,7 +119,7 @@ botao_atualizar_planilha.pack(pady=5)
 
 label_desenvolvedor = customtkinter.CTkLabel(app, 
                                             text="Desenvolvido por Rafael Souza - (41)99821-5865",
-                                            text_color="green") #cor de fundo branca
+                                            text_color="green") 
 label_desenvolvedor.place(relx=0.02, rely=0.95, anchor="sw")
 
 frame_mensagens = customtkinter.CTkFrame(app, height=50)
@@ -113,5 +127,9 @@ frame_mensagens.pack()
 
 app.update()
 app.geometry(f"{app.winfo_width()}x{app.winfo_height()}")
+
+# Bloquear o redimensionamento da janela
+app.minsize(650, 350)  # Define o tamanho mínimo
+app.maxsize(650, 350)  # Define o tamanho máximo (igual ao mínimo para bloquear o redimensionamento)
 
 app.mainloop()
